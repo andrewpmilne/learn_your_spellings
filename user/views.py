86d1372eb76118939ab9711935c87b7ccc3b91bc
andrewpmilne <andrew_p_milne@yahoo.co.uk>
2026-01-20 16:48:35 +0000

    create template

--- user/views.py
+++ user/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+# Home page
+def home_view(request):
+    return render(request, "home.html")
+
+
+# Login page
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            # Temporary redirect to home; later we’ll redirect by role
+            return redirect("/")
+    return render(request, "user/login.html")
